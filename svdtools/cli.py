d28b70b521bca2530a20ac47a242ea2e786a8930
Adam Greig <adam@adamgreig.com>
2020-03-08 23:35:43 +0000

    Add makedeps script

--- svdtools/cli.py
+++ svdtools/cli.py
@@ -17,10 +17,19 @@ def patch(yaml_file):
 
 
 @click.command()
+@click.argument("yaml-file")
+@click.argument("deps-file")
+def makedeps(yaml_file, deps_file):
+    """Generate Make dependency file listing dependencies for a YAML file."""
+    svdtools.makedeps.main(yaml_file, deps_file)
+
+
+@click.command()
 def version():
     """Version of svdtools library and tool."""
     print(svdtools.__version__)
 
 
 svdtools_cli.add_command(patch)
+svdtools_cli.add_command(makedeps)
 svdtools_cli.add_command(version)
