43e6f588e569ffefbe5576c57bcc67a33537e359
Adam Greig <adam@adamgreig.com>
2020-03-15 18:56:07 +0000

    Restore 'version' command

--- svdtools/cli.py
+++ svdtools/cli.py
@@ -25,5 +25,12 @@ def makedeps(yaml_file, deps_file):
     svdtools.makedeps.main(yaml_file, deps_file)
 
 
+@click.command()
+def version():
+    """Version of svdtools library and tool."""
+    print(svdtools.__version__)
+
+
 svdtools_cli.add_command(patch)
 svdtools_cli.add_command(makedeps)
+svdtools_cli.add_command(version)
