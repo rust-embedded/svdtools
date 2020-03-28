88ec2e12b15893f63a0d32dc7d6789d4d7e5906a
MarcoIeni <marcoieni94@gmail.com>
2020-03-28 16:40:57 +0100

    interrupts command implemented (to be improved)
    Functionality is ok, but error handling could be better

--- src/interrupts.rs
+++ src/interrupts.rs
@@ -1,8 +1,8 @@
-use std::path::PathBuf;
+use quick_xml::de;
+use serde::Deserialize;
 use std::fs::File;
 use std::io::BufReader;
-use serde::Deserialize;
-use quick_xml::de;
+use std::path::PathBuf;
 
 #[derive(Deserialize, Debug)]
 struct Svd {
@@ -23,7 +23,7 @@ struct Peripheral {
 #[derive(Deserialize, Debug)]
 struct Interrupt {
     name: String,
-    description: String,
+    description: Option<String>,
     value: u32,
 }
 
@@ -32,7 +32,46 @@ pub fn parse_device(svd_file: PathBuf, gaps: bool) {
     let file = File::open(svd_file).expect("svd file doesn't exist");
     let reader = BufReader::new(file);
 
-    let peripherals: Svd = de::from_reader(reader).expect("svd not formatted correctly");
+    let svd: Svd = de::from_reader(reader).expect("svd not formatted correctly");
 
-    println!("{:#?}", peripherals);
+    let peripheral_list = svd.peripherals.peripheral;
+    let mut interrupt_list: Vec<_> = peripheral_list
+        .into_iter()
+        .filter(|p| p.interrupt.is_some())
+        .flat_map(|p| {
+            let name = p.name;
+            p.interrupt
+                .unwrap()
+                .into_iter()
+                .map(move |i| (name.clone(), i))
+        })
+        .collect();
+    interrupt_list.sort_by_key(|i| i.1.value);
+
+    for (peripheral, interrupt) in &interrupt_list {
+        let description: String = match &interrupt.description {
+            Some(desc) => desc.clone(),
+            None => "".to_string(),
+        };
+        let description = description.replace("\r\n", " ").replace("\n", " ");
+        println!(
+            "{} {}: {} (in {})",
+            interrupt.value, interrupt.name, description, peripheral
+        );
+    }
+
+    if gaps {
+        let mut gaps = Vec::new();
+        let mut interrupt_list_iter = interrupt_list.iter().peekable();
+        while let Some(i) = interrupt_list_iter.next() {
+            let curr_num = i.1.value;
+            if let Some(next_interrupt) = interrupt_list_iter.peek() {
+                let next_num = next_interrupt.1.value;
+                for k in (curr_num + 1)..next_num {
+                    gaps.push(k);
+                }
+            }
+        }
+        println!("Gaps: {:?}", gaps);
+    }
 }
