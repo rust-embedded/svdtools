53de6f549500c66bc2729e4232a9e29596c5477e
MarcoIeni <marcoieni94@gmail.com>
2020-03-30 13:45:19 +0200

    interrupts: add test for svd_reader

--- src/interrupt/svd_reader.rs
+++ src/interrupt/svd_reader.rs
@@ -19,13 +19,14 @@ struct PeripheralXml {
     interrupt: Option<Vec<Interrupt>>,
 }
 
-#[derive(Deserialize, Debug)]
+#[derive(Deserialize, Debug, PartialEq)]
 pub struct Interrupt {
     pub name: String,
     pub description: Option<String>,
     pub value: u32,
 }
 
+#[derive(Debug, PartialEq)]
 pub struct Peripheral {
     pub name: String,
     pub interrupt: Vec<Interrupt>,
@@ -48,4 +49,59 @@ pub fn peripherals_with_interrupts<R: Read>(svd: R) -> impl Iterator<Item = Peri
             None
         }
     })
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    static SVD: &str =
+"<device>
+    <name>Test Device</name>
+    <peripherals>
+        <peripheral>
+            <name>PeriphA</name>
+            <interrupt>
+                <name>INT_A1</name>
+                <description>Interrupt A1</description>
+                <value>1</value>
+            </interrupt>
+        </peripheral>
+        <peripheral>
+            <name>PeriphB</name>
+            <interrupt>
+                <name>INT_B3</name>
+                <description>Interrupt B3</description>
+                <value>3</value>
+            </interrupt>
+        </peripheral>
+    </peripherals>
+</device>
+";
+
+    #[test]
+    fn verify_average() {
+        let svd = SVD.as_bytes();
+        let actual_peripherals: Vec<Peripheral> = peripherals_with_interrupts(svd).collect();
+        let expected_peripherals = vec![
+            Peripheral {
+                name: "PeriphA".to_string(),
+                interrupt: vec![Interrupt {
+                    name: "INT_A1".to_string(),
+                    description: Some("Interrupt A1".to_string()),
+                    value: 1,
+                }]
+            },
+            Peripheral {
+                name: "PeriphB".to_string(),
+                interrupt: vec![Interrupt {
+                    name: "INT_B3".to_string(),
+                    description: Some("Interrupt B3".to_string()),
+                    value: 3,
+                }]
+            }
+        ];
+
+        assert_eq!(actual_peripherals, expected_peripherals);
+    }
 }
