41ab0924a11759c153cfd847e066a3121153015b
MarcoIeni <marcoieni94@gmail.com>
2020-04-18 19:53:00 +0200

    tests/svd_parser: do not write to file

--- tests/svd_parser.rs
+++ tests/svd_parser.rs
@@ -1,7 +1,5 @@
-use std::io::Write;
-use std::{fs::File, path::Path};
+use std::path::Path;
 use svdtools::common::svd_reader;
-use tempfile;
 
 // test svd parser lib consistency
 #[test]
@@ -11,14 +9,10 @@ fn read_and_write() {
     let svd_path = res_dir.join("stm32l4x2.svd");
     let svd = svd_reader::device(&svd_path).unwrap();
 
-    // write the svd in another file
-    let out_dir = tempfile::tempdir().unwrap();
-    let write_path = out_dir.path().join("stm32l4x2_duplicate.svd");
-    let xml_out = svd_parser::encode(&svd).unwrap();
-    let mut out_file = File::create(&write_path).unwrap();
-    out_file.write_all(xml_out.as_bytes()).unwrap();
+    // write the svd in xml
+    let _xml = svd_parser::encode(&svd).unwrap();
 
     // read again the svd
     // BUG in svd_parser crate probably: this panics
-    // let wrote_svd = svd_reader::device(&write_path).unwrap();
+    // let same_svd = svd_parser::parse(&xml).unwrap();
 }
