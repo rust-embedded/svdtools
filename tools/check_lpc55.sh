dc1f4c11a14141a02cc2b00bdd83d1a62685317e
Nicolas Stalder <n@stalder.io>
2021-02-05 18:21:54 +0100

    Run cargo-check over the LPC55 PAC

--- tools/check_lpc55.sh
+++ tools/check_lpc55.sh
@@ -7,6 +7,6 @@ make install-svd2rust-form-rustfmt
 git clone https://github.com/lpc55/lpc55-pac --depth 1
 
 make -C lpc55-pac/ patch generate
-make -C lpc55-pac/ generate
+(cd lpc55-pac && cargo check)
 
 rm -rf lpc55-pacs
